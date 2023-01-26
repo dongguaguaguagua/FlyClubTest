51efc61a5f5c564ccd8ca163f1e060f9c38d70c5
胡宗尧 <huzongyao@192.168.1.6>
2023-01-26 20:18:19 +0800

    visualize dependency with networkx

--- project3Advanced/Project3AdvancedDoc.md
+++ project3Advanced/Project3AdvancedDoc.md
@@ -59,3 +59,60 @@ def save_to_csv():
 
 主要在`visualize_dependency.py`中实现。
 
+```python
+def get_dependency_data() -> int:
+# 粗略获取项目依赖（只爬取根目录下的requirements.txt）
+
+def get_complete_data() -> int:
+# 获取项目所有的依赖（爬取insight -> dependency中的所有数据）
+
+def draw_dependency_map() -> None:
+# 绘制依赖关系图
+```
+
+其中`get_dependency_data()`只爬取**根目录下**的`requirements.txt`，爬取速度快。但是爬下来的内容不全，因为有些requirement放在某项目目录下，而且还有各种`setup.py`中也可以定义依赖关系。这个函数会将结果以json形式保存到data文件夹下。
+
+爬取结果：`data/dependency_data.json` \[6684\]行
+
+而`get_complete_data()`能够**获取项目所有的依赖**（爬取insight->dependency中的所有数据），爬取速度较慢。它也会将结果以json形式保存到data文件夹下。
+
+爬取结果：`data/complete_dependency_data.json` \[31629\]行
+
+`draw_dependency_map()`函数使用[networkx库](https://github.com/networkx/networkx)作为绘图引擎，将读入的json文件转化成依赖关系图。
+
+```python
+pos = nx.arf_layout(G)
+nx.draw_networkx_edges(G, pos, edgelist=el, width=star_list[index] / 100000, alpha=0.5, edge_color='b')
+nx.draw_networkx_nodes(G, pos, alpha=0.5, node_size=100, edgecolors="b", node_color="w", linewidths=0.5)
+nx.draw_networkx_labels(G, pos, font_size=5, font_color="r", font_family=["Fira Code"], font_weight="bold")
+```
+
+这4行代码负责绘图，由于用下来arf_layout图最清晰，接下来的图像都用的是arf_layout。
+
+有向边（出度入度）用的是**0.5透明度的蓝色线条**，根据项目星标数多少变化粗细。
+
+节点用的是**0.5透明度的白底蓝框圆圈**，线条粗0.5，因为这样好看。
+
+最后label用的是Fira Code等宽字体，并红色加粗便于辨认。
+
+1. 用粗略获取的项目依赖数据绘制而成的图像：
+
+（线条越粗说明star越多）
+
+[Figure3](https://github.com/dongguaguaguagua/FlyClubTest/blob/main/project3Advanced/images/Figure3.png)
+
+2. 用完整版的数据：
+
+- Fira Code常规体 Top100
+
+[Figure4](https://github.com/dongguaguaguagua/FlyClubTest/blob/main/project3Advanced/images/Figure4.png)
+
+- Fira Code粗体 Top100
+
+[Figure5](https://github.com/dongguaguaguagua/FlyClubTest/blob/main/project3Advanced/images/Figure5.png)
+
+- Top200开开眼
+
+[Figure6](https://github.com/dongguaguaguagua/FlyClubTest/blob/main/project3Advanced/images/Figure5.png)
+
+但是好像两者的数据差异并非肉眼可见，那是因为我在代码里滤去了没有进入top100的项目，因此绝大部分依赖都没用上。
--- project3Advanced/visualize_dependency.py
+++ project3Advanced/visualize_dependency.py
@@ -1,30 +1,43 @@
 import os
+import re
 import json
 import time
-import requests
 import random
+import requests
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+
 
 session = requests.session()
 
 fileDir = os.path.dirname(os.path.abspath(__file__))
 
 github_raw_url = "https://raw.githubusercontent.com"
-
+github_root_url = "https://github.com"
 http_head = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33"
 }
 
-index = 1
 
-dependency = {}
-for repo in open(f"{fileDir}/data/python_repository_list.txt"):
-    time.sleep(random.random())
-    repo = repo.strip()
-    dependency[repo] = []
-    print("working on ->", repo)
-    href = github_raw_url + repo + "/master/requirements.txt"
-    requirements = session.get(href, headers=http_head)
-    if requirements.status_code == 200:
+def get_dependency_data() -> int:
+    # 粗略获取项目依赖（只爬取根目录下的requirements.txt）
+    dependency = {}
+    for repo in open(f"{fileDir}/data/python_repository_list.txt"):
+        time.sleep(random.random())
+        repo = repo.strip()
+        dependency[repo] = []
+        print("working on ->", repo)
+        href = github_raw_url + repo + "/master/requirements.txt"
+        try:
+            requirements = session.get(href, headers=http_head)
+        except requests.exceptions.ConnectionError:
+            print("Internet ConnectionError.")
+            return -2
+        if requirements.status_code != 200:
+            print("failed! status_code isn't 200.")
+            return -1
         for line in requirements.content.decode("utf-8", "ignore").split('\n'):
             line = line.strip().replace("\x00", "")
             if line == "" or (not 'a' <= line[0] <= 'z'):
@@ -34,10 +47,95 @@ for repo in open(f"{fileDir}/data/python_repository_list.txt"):
                 if pos != -1:
                     line = line[:pos].strip()
             dependency[repo].append(line)
-    else:
-        print("failed! status_code isn't 200.")
+    # print(dependency)
+    with open(f'{fileDir}/data/dependency_data.json', 'w', encoding='utf-8') as f:
+        json.dump(dependency, f, ensure_ascii=True, indent=4)
+    return 0
 
-# print(dependency)
 
-with open('dependency_data.json', 'w', encoding='utf-8') as f:
-    json.dump(dependency, f, ensure_ascii=True, indent=4)
+def get_complete_data() -> int:
+    # 获取项目所有的依赖（爬取insight->dependency中的所有数据）
+    dependency = {}
+    for repo in open(f"{fileDir}/data/python_repository_list.txt"):
+        # repo = "/dongguaguaguagua/FlyClubTest"
+        repo = repo.strip()
+        item_set = set([])
+        time.sleep(random.random())
+        print("working on ->", repo)
+        href = github_root_url + repo + "/network/dependencies"
+        while True:
+            try:
+                requirements = session.get(href, headers=http_head)
+            except requests.exceptions.ConnectionError:
+                print("Internet ConnectionError.\nRetring...")
+                continue
+            if requirements.status_code != 200:
+                print("failed! status_code isn't 200.")
+                return -1
+            break
+        html_text_all = requirements.text
+        soup = BeautifulSoup(requirements.text, "lxml")
+        load_more_dependencies = soup.find_all(class_="ajax-pagination-form")
+        for button in load_more_dependencies:
+            url = github_root_url + button['action'].strip()
+            print("GET", url)
+            http_more = session.get(url, headers=http_head)
+            html_text_all += http_more.text
+        save = False
+        for item in re.findall(r"href=\"/[a-zA-Z0-9_-]*/[a-zA-Z0-9_-]*\"", html_text_all):
+            item = item[6:-1]
+            if save and item != repo:
+                item_set.add(item)
+                continue
+            if item == repo:
+                save = True
+        dependency[repo] = list(item_set)
+    # print(dependency[repo])
+    with open(f'{fileDir}/data/complete_dependency_data.json', 'w', encoding='utf-8') as f:
+        json.dump(dependency, f, ensure_ascii=True, indent=4)
+    return 0
+
+
+def draw_dependency_map(rank=100) -> None:
+    # 绘制依赖关系图
+    csv = pd.read_csv(f"{fileDir}/data/python_repository_list.csv", encoding="utf8")
+    star_list = list(csv['星标数(star)'][:rank])
+    repo_name = list(csv['名称'][:rank])
+    G = nx.MultiDiGraph()  # 有多重边有向图
+    edge_list = []
+
+    with open(f"{fileDir}/data/dependency_data.json") as f:
+        dependency = json.load(f)
+    for key, value in dependency.items():
+        if value == []:
+            continue
+        el = []
+        key = key.split('/')[-1]
+        for item in value:
+            item = item.split('/')[-1]
+            if item in repo_name:
+                el.append((key, item))
+        G.add_edges_from(el)
+        edge_list.append(el)
+        rank -= 1
+        if rank == 0:
+            break
+    # 用下来arf_layout最清晰
+    pos = nx.arf_layout(G)
+    # pos = nx.circular_layout(G)
+    # pos = nx.random_layout(G)
+    # pos = nx.spring_layout(G)
+
+    for index, el in enumerate(edge_list):
+        nx.draw_networkx_edges(G, pos, edgelist=el, width=star_list[index] / 100000, alpha=0.5, edge_color='b')
+    nx.draw_networkx_nodes(G, pos, alpha=0.5, node_size=100, edgecolors="b", node_color="w", linewidths=0.5)
+    nx.draw_networkx_labels(G, pos, font_size=5, font_color="r", font_family=["Fira Code"], font_weight="bold")
+    plt.title(f'Github Python Topic top{index + 1}')
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == '__main__':
+    # get_dependency_data()
+    # get_complete_data()
+    draw_dependency_map(rank=200)
